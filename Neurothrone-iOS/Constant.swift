aae9d65ac86aeb6913dc08182c53d74c102b6de3
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2022-11-28 14:12:48 +0100

    add color themes and apply them

---
+++ Neurothrone-iOS/Constant.swift
@@ -0,0 +1,15 @@
+//
+//  Constant.swift
+//  Neurothrone-iOS
+//
+//  Created by Zaid Neurothrone on 2022-11-28.
+//
+
+import Foundation
+
+enum Constant {
+  struct Color {
+    static let primaryBackgroundColor = "PrimaryBackgroundColor"
+    static let secondaryBackgroundColor = "SecondaryBackgroundColor"
+  }
+}
--- Neurothrone-iOS/ContentView.swift
+++ Neurothrone-iOS/ContentView.swift
@@ -7,15 +7,70 @@
 
 import SwiftUI
 
-struct ContentView: View {
-  var body: some View {
-    VStack {
-      Image(systemName: "globe")
-        .imageScale(.large)
-        .foregroundColor(.accentColor)
-      Text("Hello, world!")
+private extension Tab {
+  var systemImage: String {
+    switch self {
+    case .home:
+      return "house"
+    case .about:
+      return "person.fill"
+    case .contact:
+      return "envelope.fill"
+    case .cv:
+      return "note.text"
+    case .portfolio:
+      return "briefcase.fill"
     }
-    .padding()
+  }
+  
+  @ViewBuilder
+  var screen: some View {
+    switch self {
+    case .home:
+      HomeScreen()
+    case .about:
+      AboutScreen()
+    case .contact:
+      ContactScreen()
+    case .cv:
+      CVScreen()
+    case .portfolio:
+      PortfolioScreen()
+    }
+  }
+  
+  @ViewBuilder
+  var view: some View {
+    NavigationStack {
+      ZStack {
+        Color(Constant.Color.secondaryBackgroundColor)
+          .ignoresSafeArea()
+        
+        self.screen
+      }
+      .navigationTitle(self.rawValue)
+//      .navigationBarTitleDisplayMode(.inline)
+    }
+  }
+}
+
+struct ContentView: View {
+  @AppStorage("selectedTab")
+  private var selectedTab: Tab = .home
+  
+  var body: some View {
+    TabView(selection: $selectedTab) {
+      Group {
+        ForEach(Tab.allCases) { tab in
+          tab.view
+            .tabItem {
+              Label(tab.rawValue, systemImage: tab.systemImage)
+            }
+        }
+      }
+      .toolbarBackground(.visible, for: .tabBar)
+      .toolbarBackground(Color(Constant.Color.primaryBackgroundColor), for: .tabBar)
+    }
   }
 }
 
