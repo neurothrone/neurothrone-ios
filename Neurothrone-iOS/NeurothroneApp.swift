a9609735b9e9ff0a50746011eb76d3f805ffe4c9
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2022-11-28 08:49:47 +0100

    set up project

---
+++ Neurothrone-iOS/NeurothroneApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeurothroneApp.swift
+//  Neurothrone-iOS
+//
+//  Created by Zaid Neurothrone on 2022-11-28.
+//
+
+import SwiftUI
+
+@main
+struct NeurothroneApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
